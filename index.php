5312c9dafa8908a89067c438bd29ca17dff8e49a
mark <mark.kors@gmail.com>
2020-04-17 11:07:33 +0200

    Part 1

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+$submitmessage = "Log In";
+$bottommessage = <<< TEXT
+        <div>
+            <p>Not an account?, <a href="index.php?action=create">create</a> one...</p>
+        </div>
+TEXT;
+$secondpassword = null;
+
+
+if(isset($_GET['action'])) {
+    $action = $_GET['action'];
+    switch($action) {
+        case 'create':
+            $submitmessage = "Create";
+            $bottommessage = "";
+            $secondpassword = "<input type=\"password\" id=\"password2\" name=\"password2\" placeholder=\"retype password\">";
+            break;
+
+    }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="css/styles.css" type="text/css" rel="stylesheet"/>
+
+    <title>Login</title>
+</head>
+<body>
+<div class="container">
+    <div id="formContent">
+        <form action="index.php" method="post">
+            <input type="text" id="login"name="login" placeholder="login">
+            <input type="password" id="password" name="password" placeholder="password">
+            <?=$secondpassword?>
+            <input type="submit" class="fadeIn fourth" value="<?=$submitmessage?>">
+        </form>
+        <?=$bottommessage?>
+</div>
+
+</div>
+</body>
+</html>
